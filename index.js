c026972029407db14bb307b6cb26540017b977f3
van-matos <vanmatos@poli.ufrj.br>
2022-06-15 18:12:26 -0300

    Exercício completo

---
+++ index.js
@@ -0,0 +1,53 @@
+import express from 'express';
+
+const app = express();
+
+app.get("/holidays", (req, res) => {
+    const holidays = [
+        { date: "1/1/2022", name: "Confraternização mundial" },
+        { date: "1/3/2022", name: "Carnaval" },
+        { date: "4/17/2022", name: "Páscoa" },
+        { date: "4/21/2022", name: "Tiradentes" },
+        { date: "5/1/2022", name: "Dia do trabalho" },
+        { date: "6/16/2022", name: "Corpus Christi" },
+        { date: "9/7/2022", name: "Independência do Brasil" },
+        { date: "10/12/2022", name: "Nossa Senhora Aparecida" },
+        { date: "11/2/2022", name: "Finados" },
+        { date: "11/15/2022", name: "Proclamação da República" },
+        { date: "12/25/2022", name: "Natal" }
+      ];
+
+    res.send(holidays);
+})
+
+app.get("/is-today-holiday", (req, res) => {
+    const holidays = [
+        { date: "1/1/2022", name: "Confraternização mundial" },
+        { date: "1/3/2022", name: "Carnaval" },
+        { date: "4/17/2022", name: "Páscoa" },
+        { date: "4/21/2022", name: "Tiradentes" },
+        { date: "5/1/2022", name: "Dia do trabalho" },
+        { date: "6/16/2022", name: "Corpus Christi" },
+        { date: "9/7/2022", name: "Independência do Brasil" },
+        { date: "10/12/2022", name: "Nossa Senhora Aparecida" },
+        { date: "11/2/2022", name: "Finados" },
+        { date: "11/15/2022", name: "Proclamação da República" },
+        { date: "12/25/2022", name: "Natal" }
+      ];
+
+    const today = new Date();
+    console.log(today.toLocaleDateString()); // 1/1/2022
+
+    const message = "Não, hoje não é feriado";
+
+    for (let i = 0; i < holidays.length; i++) {
+        console.log(holidays[i].date);
+        if (today.toLocaleDateString() === holidays[i].date) {
+            message = `Sim! Hoje é ${holidays[i].name}`
+        } 
+    }
+
+    res.send(message);
+})
+
+app.listen(4000);
